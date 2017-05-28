165e384d5cfb4156654b4ebbfa95ef68178a9c4c
wbenbihi <waligoo@gmail.com>
2017-05-28 18:43:48 +0200

    Update params.py

--- params.py
+++ params.py
@@ -13,10 +13,10 @@ Created on Sun May 28 17:39:00 2017
 
 # Directory (do not forget the last '/')
 
-img_dir 			= '~/images/'		# Directory of image dataset (.png, .jpg)
-data_dir 			= '~/data/'		# Directory of .csv files
-train_dir 		= '~/logs/train/'		# Path to save training logs
-test_dir 			= '~/logs/test/'		# Path to save testing logs
+img_dir 			= '~/images/'		  # Directory of image dataset (.png, .jpg)
+data_dir 			= '~/data/'		    # Directory of .csv files
+train_dir 		= '~/logs/train/'	# Path to save training logs
+test_dir 			= '~/logs/test/'	# Path to save testing logs
 processed_dir 	= '~/arrays/'		# Directory of processed images (.npy)
 
 # TensorFlow Parameters
@@ -27,13 +27,13 @@ cpu = '/cpu:0'			# Indicates which CPU to use (only use CPU)
 
 # Training Parameters
 
-learning_rate 		= 2.5e-4 	# Learning Rate 
-nEpochs 				= 30		# Number of epochs
-iter_by_epoch 		= 1000		# Number of batch to train in one epoch
-batch_size 			= 16		# Batch Size per iteration
-limit_train_test 	= 24000	# Index of separation between training and testing set
+learning_rate 		= 2.5e-4 	  # Learning Rate 
+nEpochs 				  = 30		    # Number of epochs
+iter_by_epoch 		= 1000		  # Number of batch to train in one epoch
+batch_size 			  = 16		    # Batch Size per iteration
+limit_train_test 	= 24000	    # Index of separation between training and testing set
 
-step_to_save 		= 500		# Step to save summaries on TensorBoard (should be lower than iter_by_epoch)
+step_to_save 		  = 500	  # Step to save summaries on TensorBoard (should be lower than iter_by_epoch)
 random_angle_max 	= 30		# Max of random rotation
 random_angle_min 	= -30		# Min of random rotation
 
