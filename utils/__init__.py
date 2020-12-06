3bbff76d25755e3e92e6ec26ed079329ec39a30b
wbenbihi <waligoo@gmail.com>
2020-12-06 02:16:19 +0100

    [ADD] yaml parsing

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,16 @@
-from utils.argmax_funcs import argmax2D_with_batch, argmax2D_without_batch
+import os
+import yaml
+import dacite
+from utils.config import HourglassConfig
+
+ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+
+def load_configuration(config:str):
+    # Open Config Yaml File
+    with open(config, 'r') as f:
+        yaml_dict = yaml.load(f, Loader=yaml.FullLoader)
+    
+    # Parse Configuration / Data Validation
+    cfg = dacite.from_dict(HourglassConfig, yaml_dict)
+    return cfg
