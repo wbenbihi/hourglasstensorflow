d31bc98b50ea13f6cce6ebf52f381bb5e66c76b7
wbenbihi <waligoo@gmail.com>
2022-08-17 17:26:52 +0800

    [ADD] Access mpii main function/models in parsers

--- hourglass_tensorflow/utils/parsers/__init__.py
+++ hourglass_tensorflow/utils/parsers/__init__.py
@@ -1,1 +1,10 @@
+from mpii import MPIIAct
+from mpii import MPIIDataset
+from mpii import MPIIAnnorect
+from mpii import MPIIAnnoPoint
+from mpii import MPIIDatapoint
+from mpii import MPIIAnnotation
+from mpii import parse_mpii
+from mpii import save_parsed_mpii
+
 from . import mpii
