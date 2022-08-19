9df51a5cbdf341d2f40d42194738f48455c75412
wbenbihi <waligoo@gmail.com>
2022-08-19 15:46:36 +0800

    [ADD][FEAT] automated import from configuration

---
+++ hourglass_tensorflow/utils/parsers/_parse_import.py
@@ -0,0 +1,37 @@
+import importlib
+from typing import Type
+from typing import Callable
+
+from hourglass_tensorflow._errors import BadConfigurationError
+from hourglass_tensorflow.datasets import HTFBaseDatasetHandler
+
+
+def _get_object(access_string) -> Type:
+    """Get the currently-configured HTFDataset class
+    :raises ImproperConfigurationError: if ImportError or AttributeError is raised
+    :returns: HTFDataset class
+    """
+
+    try:
+        # All except the last part is the import path
+        parts = access_string.split(".")
+        module = ".".join(parts[:-1])
+        # The final part is the name of the parse function
+        return getattr(importlib.import_module(module), parts[-1])
+    except (ImportError, AttributeError) as error:
+        raise BadConfigurationError(f'Unable to import HTFDataset "{error}"')
+
+
+def _generate_constraint_object_getter(type: Type) -> Callable[[str], Type]:
+    def get_object(access_string: str) -> Type[type]:
+        obj: type = _get_object(access_string)
+        if not issubclass(obj, type):
+            raise TypeError(f"{obj} is not a subclass of {type}")
+        return obj
+
+    return get_object
+
+
+get_dataset: Callable[
+    [str], Type[HTFBaseDatasetHandler]
+] = _generate_constraint_object_getter(type=HTFBaseDatasetHandler)
