d9bb4290523370502b6d8d5850ec89a5349d19a6
wbenbihi <waligoo@gmail.com>
2022-08-20 15:00:13 +0800

    [FIX] Make input_size primitive type

--- hourglass_tensorflow/utils/config.py
+++ hourglass_tensorflow/utils/config.py
@@ -68,17 +68,13 @@ class HTFDatasetBBoxConfig(BaseModel):
     )
 
 
-class HTFDatasetSizeConfig(BaseModel):
-    input: int = 256
-
-
 class HTFDatasetConfig(BaseModel):
     object: str
+    input_size: int = 256
     params: Optional[HTFDatasetParamsConfig] = Field(
         default_factory=HTFDatasetParamsConfig
     )
     sets: Optional[HTFDatasetSetsConfig]
-    size: Optional[HTFDatasetSizeConfig] = Field(default_factory=HTFDatasetSizeConfig)
     split: Optional[HTFDatasetSplitConfig] = Field(
         default_factory=HTFDatasetSplitConfig
     )
