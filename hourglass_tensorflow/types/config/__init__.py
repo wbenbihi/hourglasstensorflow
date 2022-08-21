6cb338d11e69f9eff9b046d87fc265aa78c2be9b
wbenbihi <waligoo@gmail.com>
2022-08-21 21:54:00 +0800

    [REFACTO] Config Parser

---
+++ hourglass_tensorflow/types/config/__init__.py
@@ -0,0 +1,133 @@
+import enum
+import json
+from typing import Any
+from typing import Dict
+from typing import Union
+from typing import Callable
+from typing import Optional
+
+import toml
+import yaml
+
+from hourglass_tensorflow.types.config.data import HTFDataConfig
+from hourglass_tensorflow.types.config.fields import HTFConfigField
+from hourglass_tensorflow.utils.object_logger import ObjectLogger
+
+
+class HTFConfigMode(enum.Enum):
+    TEST = "test"
+    TRAIN = "train"
+    INFERENCE = "inference"
+
+
+class HTFConfig(HTFConfigField):
+    version: Optional[Union[str, int]]
+    mode: HTFConfigMode = HTFConfigMode.TRAIN
+    data: Optional[HTFDataConfig]
+    dataset: Optional[Any]
+    model: Optional[Any]
+
+
+# region ConfigParser
+
+
+class HTFConfigParser(ObjectLogger):
+    """Parse configuration files for `hourglass_tensorflow`
+
+    Currently HTFConfigParser supports `.json`, `.toml`, `.yaml`, `.yml` files.
+    The configuration file must follow the model defined by `HTFConfig`,
+    see `CONFIGURATION.md` for more details.
+
+    Args:
+        filename (str): Configuration file name
+    """
+
+    def __init__(self, filename: str, verbose: bool = True) -> None:
+        """Init ConfigParser. See `help(HTFConfigParser)`"""
+        self._verbose = verbose
+        self._filename = filename
+        self._data = {}
+        self._config: Optional[HTFConfig] = None
+        self.info("Reading configuration ...")
+        # Parse Config File
+        self._infer_source()
+        self._parse_config()
+
+    def __call__(self) -> HTFConfig:
+        """Returns the parsed configuration as a `pydantic.BaseModel` object
+
+        Returns:
+            HTFConfig: The configuration object
+        """
+        return self._config
+
+    def __repr__(self) -> str:
+        return f"<HFTConfigParser type:{self.file_type} source:{self._filename}>"
+
+    @property
+    def config(self) -> HTFConfig:
+        return self._config
+
+    @property
+    def file_type(self) -> HTFConfig:
+        return self._filename.split(".")[-1]
+
+    @classmethod
+    def parse_toml(cls, filename: str) -> Dict:
+        """Parse a `.toml` file
+
+        Args:
+            filename (str): toml file name
+
+        Returns:
+            Dict: parsed toml as `dict`
+        """
+        return toml.load(filename)
+
+    @classmethod
+    def parse_json(cls, filename: str) -> Dict:
+        """Parse a `.json` file
+
+        Args:
+            filename (str): json file name
+
+        Returns:
+            Dict: parsed json as `dict`
+        """
+        with open(filename, "r") as f:
+            return json.load(f)
+
+    @classmethod
+    def parse_yaml(cls, filename: str) -> Dict:
+        """Parse a `.yml`/`.yaml` file
+
+        Args:
+            filename (str): yaml file name
+
+        Returns:
+            Dict: parsed yaml as `dict`
+        """
+        with open(filename, "r") as f:
+            return yaml.safe_load(f)
+
+    def _infer_source(self) -> None:
+        parsers = {
+            "yml": self.parse_yaml,
+            "yaml": self.parse_yaml,
+            "json": self.parse_json,
+            "toml": self.parse_toml,
+        }
+        cfg_parser: Optional[Callable[[str], Dict]] = parsers.get(
+            self._filename.split(".")[-1]
+        )
+        if cfg_parser is None:
+            raise ValueError(
+                "The file extension is not supported. Use YAML, TOML or JSON config file"
+            )
+        self._data = cfg_parser(self._filename)
+
+    def _parse_config(self) -> None:
+        self._config = HTFConfig.parse_obj(self._data)
+
+
+# endregion
