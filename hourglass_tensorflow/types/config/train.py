d8cc33ad8f0bb6e02465569aaf0b876f0e129b5f
wbenbihi <waligoo@gmail.com>
2022-08-23 16:54:15 +0800

    [ADD](types) HTFTrainConfig

---
+++ hourglass_tensorflow/types/config/train.py
@@ -0,0 +1,20 @@
+import enum
+from typing import Dict
+from typing import List
+from typing import Union
+from typing import Literal
+from typing import Optional
+
+from pydantic import Field
+from pydantic import BaseModel
+
+from hourglass_tensorflow.types.config.fields import HTFConfigField
+from hourglass_tensorflow.types.config.fields import HTFObjectReference
+
+
+class HTFTrainConfig(HTFConfigField):
+    object: Optional[HTFObjectReference] = Field(
+        default_factory=HTFObjectReference(
+            source="hourglass_tensorflow.handlers.model.HTFModelHandler"
+        )
+    )
