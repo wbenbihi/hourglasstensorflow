8dff143355f107184a0378130aacf84bf2bd778f
wbenbihi <waligoo@gmail.com>
2022-08-25 19:31:13 +0800

    [ADD][CI](semver) Add pre-commit command

---
+++ ci/pre_commit_command.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pytest tests/
+flake8 hourglass_tensorflow --statistics --tee --output-file ./reports/flake8stats.txt
+genbadge tests -i reports/junit.xml -o - > reports/tests-badge.svg
+genbadge coverage -i reports/coverage.xml -o - > reports/coverage-badge.svg
+genbadge flake8 -i reports/flake8stats.txt -o - > reports/flake8-badge.svg
