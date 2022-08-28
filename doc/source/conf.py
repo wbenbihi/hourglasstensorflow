ba27385c1a2dd07a11d0fdf547b1956f2dc38866
wbenbihi <waligoo@gmail.com>
2022-08-28 15:32:38 +0800

    [DOC][CONFIG] Add sphinx doc

---
+++ doc/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("hourglass_tensorflow"))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Houglass Tensorflow"
+copyright = "2022, Walid BENBIHI"
+author = "Walid BENBIHI"
+release = "1.0.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.coverage",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+source_suffix = [".md"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
