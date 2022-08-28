6a70ea2099c4c5994b0694d94dab0ba88b54fa00
wbenbihi <waligoo@gmail.com>
2022-08-28 23:58:23 +0800

    [ADD][DOC] docstrings utils

--- hourglass_tensorflow/utils/sets/__init__.py
+++ hourglass_tensorflow/utils/sets/__init__.py
@@ -5,7 +5,15 @@ from typing import Tuple
 
 
 def split_train_test(items: Set[str], ratio: float = 0.8) -> Tuple[Set[str], Set[str]]:
+    """Helper function to split a set of item according to a ratio
 
+    Args:
+        items (Set[str]): set of item to split
+        ratio (float, optional): ratio to apply for splitting. Defaults to 0.8.
+
+    Returns:
+        Tuple[Set[str], Set[str]]: splitted sets
+    """
     length = len(items)
     items_to_select = int(length * ratio)
     train_samples = set(random.sample(items, items_to_select))
